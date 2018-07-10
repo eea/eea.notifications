406816ac32e929432dd9e4f97e1ed6cfdd9fbe51
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-07-10 08:35:30 +0300

    [refs #91506] Add notifications center view.

--- eea/notifications/browser/notifications_center.py
+++ eea/notifications/browser/notifications_center.py
@@ -1,6 +1,31 @@
 from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone import api
 
 
-# class TestView(BrowserView):
-#     """ Test View
-#     """
+def notifications_center(site):
+    """ The async script responsible for processing the pings and notify users
+    """
+    print "ZZZ Notified 1"
+    print "ZZZ Notified 2"
+    print "ZZZ Notified 3"
+
+
+class NotificationsCenter(BrowserView):
+    """ Notifications Center as view
+    """
+    index = ViewPageTemplateFile("templates/notifications_center.pt")
+
+    def render(self):
+        return self.index()
+
+    @property
+    def test_value(self):
+        return "Test value"
+
+    def __call__(self):
+        site = api.portal.get()
+
+        notifications_center(site)
+
+        return self.render()
