6ba2a886c59c96a5736b9df27f10fdf191a51863
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-07-16 11:22:28 +0300

    [refs #91506] WIP user's tags form.

--- eea/notifications/browser/user_tags_form.py
+++ eea/notifications/browser/user_tags_form.py
@@ -8,6 +8,31 @@ class UserTagsForm(BrowserView):
         User subscribes to a list of tags (from all content tags available).
         """
 
+    def get_tags(self):
+        """ The list of available content tags
+        """
+        # [TODO] WIP
+        return [
+            ('education', 'Education'),
+            ('security', 'Security'),
+            ('agriculture', 'Agriculture'),
+            ('books', 'Books'),
+            ('lorem-ipsum', 'Lorem ipsum')
+        ]
+
+    def set_tags(self, tags):
+        """ Save user preferences
+        """
+        # [TODO] WIP
+        print "Saved: {0}".format(tags)
+
     def __call__(self):
-        print "ZZZ"
+        if "submit" in self.request.form:
+            tags = []
+            value = self.request.form.get('tags-list', [])
+            if isinstance(value, basestring):
+                tags.append(value)
+            else:
+                tags = value
+            self.set_tags(tags)
         return self.index()
