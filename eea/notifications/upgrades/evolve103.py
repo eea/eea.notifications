283270cca6a78d3bde457f52c880fd6695880481
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-07-18 10:52:32 +0300

    [refs #91506] WIP catalog configuration - add lexicon.

--- eea/notifications/upgrades/evolve103.py
+++ eea/notifications/upgrades/evolve103.py
@@ -11,6 +11,42 @@ def run(context):
             index = catalog._catalog.getIndex('getTags')
             index = index  # The Happy PEP
         except KeyError:
+            class Empty(object):
+                """ Empty """
+                pass
+
+            elem = []
+
+            word_splitter = Empty()
+            word_splitter.group = 'Word Splitter'
+            word_splitter.name = 'HTML aware splitter'
+
+            case_normalizer = Empty()
+            case_normalizer.group = 'Case Normalizer'
+            case_normalizer.name = 'Case Normalizer'
+
+            stop_words = Empty()
+            stop_words.group = 'Stop Words'
+            stop_words.name = 'Remove listed and single char words'
+
+            elem.append(word_splitter)
+            elem.append(case_normalizer)
+            elem.append(stop_words)
+
+            catalog.manage_addProduct['ZCTextIndex'].manage_addLexicon(
+                'plone_lexicon', 'Default Lexicon', elem)
+
+            title_extras = Empty()
+            title_extras.doc_attr = 'Title'
+            title_extras.index_type = 'Okapi BM25 Rank'
+            title_extras.lexicon_id = 'plone_lexicon'
+
+            catalog.addIndex('portal_type', 'FieldIndex')
+            catalog.addIndex('Title', "ZCTextIndex", title_extras)
+
+            catalog.addColumn('portal_type')
+            catalog.addColumn('Title')
+
             catalog.addIndex('getTags', 'FieldIndex')
             catalog.reindexIndex(('getTags'), REQUEST=None)
             LOGGER.info("Added 'getTags' index to eea_notifications_catalog")
