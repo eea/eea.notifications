9c18773758c508d15fb477ef9287dc7dabafdc88
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-07-19 15:24:53 +0300

    [refs #91506] Catalog edited objects using subsciber event.

--- eea/notifications/content/events.py
+++ eea/notifications/content/events.py
@@ -1,2 +1,18 @@
 """ Events
 """
+from eea.notifications.catalogtool import get_catalog
+from plone import api
+
+
+def update_catalog(obj, event):
+    """ Reindex object in EEA Notifications Catalog
+    """
+    site = api.portal.get()
+    eea_notifications_catalog = get_catalog(site)
+    eea_notifications_catalog.catalog_object(
+        obj,
+        idxs=(
+            'getTags',
+        ),
+        update_metadata=1
+    )
