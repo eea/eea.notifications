a08517659c70fdf19fad110af48a16aa5b5d46bd
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-07-24 19:14:39 +0300

    [refs #91506] Detalied events actions.

--- eea/notifications/content/events.py
+++ eea/notifications/content/events.py
@@ -1,6 +1,7 @@
 """ Events
 """
 from eea.notifications.catalogtool import get_catalog
+from eea.notifications.utils import LOGGER
 
 
 def add_or_update_in_catalog(obj, event):
@@ -19,3 +20,31 @@ def remove_from_catalog(obj, event):
     """ Remove object from EEA Notifications Catalog
     """
     get_catalog().uncatalog_object(obj.absolute_url_path())
+
+
+def object_modified(obj, event):
+    """ Content was modified
+    """
+    add_or_update_in_catalog(obj, event)
+    LOGGER.info("ZZZ Content was modified.")
+
+
+def object_moved(obj, event):
+    """ Content was moved
+    """
+    add_or_update_in_catalog(obj, event)
+    LOGGER.info("ZZZ Content was moved.")
+
+
+def object_added(obj, event):
+    """ Content was added
+    """
+    add_or_update_in_catalog(obj, event)
+    LOGGER.info("ZZZ Content was added.")
+
+
+def object_removed(obj, event):
+    """ Content was removed
+    """
+    remove_from_catalog(obj, event)
+    LOGGER.info("ZZZ Content was removed.")
