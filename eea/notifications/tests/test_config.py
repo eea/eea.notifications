34b200986c0dffcc617d4b9eb867eb11353ff754
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-08-23 14:57:37 +0300

    [refs #91506] Add tests for config.

---
+++ eea/notifications/tests/test_config.py
@@ -0,0 +1,27 @@
+import unittest
+from eea.notifications.tests.utils import log
+
+
+class TestConfig(unittest.TestCase):
+    """Unit test for the Program type
+    """
+
+    def test_env_vars(self):
+        log("test_env_vars", "Non empty env vars in configuration.")
+        from eea.notifications.config import ENV_HOST_NAME
+        from eea.notifications.config import ENV_PLONE_NAME
+        from eea.notifications.config import ANNOT_TAGS_KEY
+        from eea.notifications.config import ANNOT_EVENTS_KEY
+        from eea.notifications.config import OBJECT_EVENTS
+        from eea.notifications.config import RABBIT_QUEUE
+
+        self.assertTrue(ENV_HOST_NAME)
+        self.assertTrue(ENV_PLONE_NAME)
+        self.assertTrue(ANNOT_TAGS_KEY)
+        self.assertTrue(ANNOT_EVENTS_KEY)
+        self.assertTrue(OBJECT_EVENTS)
+        self.assertTrue(RABBIT_QUEUE)
+
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
---
+++ eea/notifications/tests/utils.py
@@ -0,0 +1,2 @@
+def log(test_name, test_info):
+    print(" ::: TESTING ::: {0}: {1}".format(test_name, test_info))
