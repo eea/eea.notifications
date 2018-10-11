88ddff687df664491040fa089d74e23d00f7cb8d
Ghiță Bizău <ghita_bizau@yahoo.com>
2018-10-11 17:22:51 +0300

    Fix non-ascii error in vocabulary creation.

--- eea/notifications/vocabulary.py
+++ eea/notifications/vocabulary.py
@@ -1,15 +1,27 @@
+from Products.CMFPlone.utils import safe_unicode
+from binascii import b2a_qp
 from eea.notifications.catalogtool import get_catalog
 from zope.interface import provider
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
+import six
 
 
-LABELS = {}
+def safe_encode(value):
+    if isinstance(value, six.text_type):
+        # no need to use portal encoding for transitional encoding from
+        # unicode to ascii. utf-8 should be fine.
+        value = value.encode('utf-8')
+    return value
+
+
+def safe_simpleterm_from_value(value):
+    return SimpleTerm(value, b2a_qp(safe_encode(value)), safe_unicode(value))
 
 
 def vocab_from_values(values):
-    terms = [SimpleTerm(x, x, LABELS.get(x, x)) for x in values]
+    terms = [safe_simpleterm_from_value(x) for x in values]
 
     try:
         terms.sort(key=lambda t: t.title)
